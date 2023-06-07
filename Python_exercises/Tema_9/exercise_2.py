4d953eaf9f6c3bd15e1e5b1c071b1fd46727436f
Axlfc <axlffcc@gmail.com>
2023-06-07 20:54:40 +0200

    UPDATED: Exercise 9.2

--- Python_exercises/Tema_9/exercise_2.py
+++ Python_exercises/Tema_9/exercise_2.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
+
+def obtener_impares(lista): 
+    resultado = list(filter(lambda x: x % 2 != 0, lista))
+    print(resultado)
+    resultado = reduce(lambda x, y: x + y, resultado)
+    print(resultado)
+
+
+lista = list(range(100))
+
+obtener_impares(lista)
