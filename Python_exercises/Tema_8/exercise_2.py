7e0151631e97202d1ae2f5f6fffa52fdfe3cf46d
Axlfc <axlffcc@gmail.com>
2023-06-07 20:48:12 +0200

    UPDATED: Exercise 8.2

--- Python_exercises/Tema_8/exercise_2.py
+++ Python_exercises/Tema_8/exercise_2.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pickle import dump, load
+
+class Vehiculo:
+    def __init__(self, color, puertas):
+        self.color = color
+        self.puertas = puertas
+
+    def __str__(self):
+        return self.color + " " + self.puertas
+
+toyota = Vehiculo("Rojo", "4")
+print(toyota)
+
+# Guardar el objeto en un archivo
+with open('vehiculo_objeto', 'w+b') as file:
+    dump(toyota, file)
+
+# Cargar el objeto desde el archivo
+with open('vehiculo_objeto', 'rb') as file:
+    nuevo_toyota = load(file)
+
+print(nuevo_toyota)
