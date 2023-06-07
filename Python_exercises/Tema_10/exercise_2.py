4f2b982d4f87e6548e26f63ce7801c16e975578a
Axlfc <axlffcc@gmail.com>
2023-06-07 21:05:47 +0200

    UPDATED: Exercise 10.2

--- Python_exercises/Tema_10/exercise_2.py
+++ Python_exercises/Tema_10/exercise_2.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from tkinter import Tk, Listbox, Label, END, StringVar
+
+master = Tk()
+elemento = StringVar()
+listbox = Listbox(master)
+for item in ["Pepe", "María", "Ernesto", "Ruben", "Carlos", "Laura", "Ana", "Lorena"]:
+    listbox.insert(END, item)
+listbox.pack()
+label = Label(text="Lista de nombres de personas")
+label.pack()
+master.mainloop()
