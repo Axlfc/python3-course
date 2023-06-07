758c022c308df58134523ed307964d8eea7cf966
Axlfc <axlffcc@gmail.com>
2023-06-07 20:38:34 +0200

    ADDED: exercise 7.1

---
+++ Python_exercises/Tema_7/calculadora.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
---
+++ Python_exercises/Tema_7/exercise_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import calculadora
+
+# Llamar a las funciones de la calculadora y mostrar los resultados por consola
+a = 2
+b = 4
+
+print("Suma:", calculadora.sumar(a, b))
+print("Resta:", calculadora.restar(a, b))
+print("Multiplicación:", calculadora.multiplicar(a, b))
+print("División:", calculadora.dividir(a, b))
